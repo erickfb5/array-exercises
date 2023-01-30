b42fa7ccc02c249047a670e382297bd23c73d612
Erick Brandao <brandaoerick@MacBook-Air.local>
2023-01-30 12:51:32 -0300

    refactored exercise 04

--- 04_lastElement.js
+++ 04_lastElement.js
@@ -1,11 +1,11 @@
 // 04. Write a JavaScript function to get the last element of an array.
 // Passing a parameter 'n' will return the last 'n' elements of the array
-const lastElement = (arr, n) => {
-  if (!n) return arr[arr.length - 1];
-  return arr.filter((elem, index) =>  index >(arr.length-1)-n && elem)
-}
+const lastElement = (arr, n) =>
+  !n
+    ? arr[arr.length - 1]
+    : arr.filter((elem, index) => index > arr.length - 1 - n && elem);
 
 console.log(lastElement([5, 4, 3, 2, 1]));
 console.log(lastElement([5, 4, 3, 2, 1], 2));
-console.log(lastElement([5, 4, 3, 2, 1],3))
-console.log(lastElement([5, 4, 3, 2, 1],4));
+console.log(lastElement([5, 4, 3, 2, 1], 3));
+console.log(lastElement([5, 4, 3, 2, 1], 4));
